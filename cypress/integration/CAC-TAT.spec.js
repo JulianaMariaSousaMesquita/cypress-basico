ed087556fbbdd006323bbcd784c741a2a79a976e
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-08-15 12:49:25 -0300

    ✨ feat: add test scenario Invalid email error message

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -19,5 +19,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible')
     })
 
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        cy.get('#firstName').type('Juliana')
+        cy.get('#lastName').type('Mesquita')
+        cy.get('#email').type('juliana@exemplo,com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
+
   })
 
