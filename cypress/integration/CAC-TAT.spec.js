ccef58af87cc0d4c22c61197ed7111278d0baaae
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-08-15 16:25:47 -0300

    ⚡ perf: improvement change where has get button to contains

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -15,7 +15,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#lastName').type('Mesquita')
         cy.get('#email').type('juliana@example.com')
         cy.get('#open-text-area').type(longText, {delay : 0})
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.success').should('be.visible')
     })
 
@@ -24,7 +24,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#lastName').type('Mesquita')
         cy.get('#email').type('juliana@exemplo,com')
         cy.get('#open-text-area').type('Teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
     })
 
@@ -38,7 +38,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#email').type('juliana@exemplo.com')
         cy.get('#phone-checkbox').click()
         cy.get('#open-text-area').type('Teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
     })
 
@@ -50,7 +50,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
     })
 
